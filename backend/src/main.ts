596a88060f1a43bf07819d7d765203e9cefeaf3e
WatcharachaiSk <watcharachai.sa@rmuti.ac.th>
2023-06-16 14:34:12 +0700

    add
    add

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cors from 'cors';
+import * as path from 'path';
+
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
@@ -10,6 +12,10 @@ async function bootstrap() {
     cors: true,
     logger: ['log', 'debug', 'error', 'warn'],
   });
+  const distPath = path.join(__dirname, '../build');
+
+  // Serve static files from the distPath directory
+  app.useStaticAssets(distPath);
   app.enableCors();
   app.setGlobalPrefix('api');
 
